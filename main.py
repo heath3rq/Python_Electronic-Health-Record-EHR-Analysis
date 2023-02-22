1a408d5e57a472d652bafff8c4f3b5a3cef3dbf3
heath3rq <105904149+heath3rq@users.noreply.github.com>
2023-02-22 18:23:33 +0000

    update time complexity analysis and patient parser data structure

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ from datetime import datetime
 
 def parse_data(
     patient_filename: str, lab_filename: str
-) -> tuple[list[dict[str, list[str]]], list[dict[str, list[str]]]]:
+) -> tuple[dict[str, list[str]], list[dict[str, list[str]]]]:
     """Parse EHR Data.
 
-    Opening the file takes constant time, but reading lines takes O(N)/O(M)
-    time for patient/lab record files,respectively. The list comprehension that
-    strips the lines of whitespace and splitting the lines into a list takes
-    O(1)*O(N) = O(N) time for patient records, and O(1)*O(M) = O(M).
+    Opening the file takes constant time, but reading lines takes O(N*CP) or
+    O(M*CL) time for patient/lab record files,respectively. Stripping the lines
+    of whitespace and splitting the lines into a list takes
+    O(CP)*O(N) = O(CP*N) time for patient records, and O(CL)*O(M) = O(CL*M).
     The list comprehension that puts each line of patient record in the list
     into a dictionary takes O(1)*O(N) = O(N) time, and O(1)*O(M) = O(M) time
     for each lab record. If we assume M > N, our big-O notation is therefore
@@ -32,20 +32,18 @@ def parse_data(
     with open(
         patient_filename, mode="r", encoding="utf-8-sig"
     ) as file:  # O(1)
-        patient_lines_str = file.readlines()  # O(N)
+        patient_lines_str = file.readlines()  # O(N*CP)
     patient_lines_lst = [
-        i.strip().split("\t") for i in patient_lines_str  # O(1)  # O(N)
+        i.strip().split("\t") for i in patient_lines_str  # O(N*CP)
     ]
-    patient_records = [
-        {r[0]: r[1:]} for r in patient_lines_lst  # O(1)  # O(N)
-    ]
+    patient_dict = {}  # O(1)
+    for record in patient_lines_lst:  # O(N)
+        patient_dict[record[0]] = record[1:]  # O(1)
     with open(lab_filename, mode="r", encoding="utf-8-sig") as file:  # O(1)
-        lab_lines_str = file.readlines()  # O(M)
-    lab_lines_lst = [
-        i.strip().split("\t") for i in lab_lines_str  # O(1)  # O(M)
-    ]
-    lab_records = [{r[0]: r[1:]} for r in lab_lines_lst]  # O(1)  # O(M)
-    return patient_records, lab_records  # O(1)
+        lab_lines_str = file.readlines()  # O(M*CL)
+    lab_lines_lst = [i.strip().split("\t") for i in lab_lines_str]  # O(M*CL)
+    lab_records = [{r[0]: r[1:]} for r in lab_lines_lst]  # O(M)
+    return patient_dict, lab_records  # O(1)
 
 
 def date_type_conversion(date_time: str) -> datetime:
@@ -56,29 +54,21 @@ def date_type_conversion(date_time: str) -> datetime:
     return datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S.%f")  # O(1)
 
 
-def patient_age(
-    patient_records: list[dict[str, list[str]]], patient_id: str
-) -> int:
+def patient_age(patient_records: dict[str, list[str]], patient_id: str) -> int:
     """Calculate Patient Age in Years.
 
-    Retriving date index from a list of headers will take time O(CP) where CP
-    is the length of the patient file header. Getting the next item of a
-    list takes O(1) time. Looping through the patient records to check if a
-    patient ID exists takes 0(N**2) time. Then, retriving patient's date of
-    birth takes O(1) time. Converting date from string format to date format,
-    retrieving current year, and calculating/returning patient age are all
-    operations of O(1) time complexity. Our big-O notation is therefore
-    O(N**2) after dropping the constant factors.
+    Retriving date index from a list of headers will take constant time O(1)
+    Retriving values from a dictionary takes O(N) time. Converting date from
+    string format to date format,retrieving current year, and
+    calculating/returning patient age are all operations of O(1) time
+    complexity. Our big-O notation is therefore O(N) after dropping
+    the constant factors.
 
     """
-    date_index = patient_records[0]["PatientID"].index(
+    date_index = patient_records["PatientID"].index(
         "PatientDateOfBirth"
-    )  # O(CP)
-    dob_string = next(  # O(1)
-        record[patient_id][date_index]  # O(1)
-        for record in patient_records  # O(N)
-        if patient_id in record  # O(N)
-    )
+    )  # O(1)
+    dob_string = patient_records[patient_id][date_index]  # O(N*CP)
     dob_int = date_type_conversion(dob_string)  # O(1)
     today = datetime.now()  # O(1)
     age = today.year - dob_int.year  # O(1)
@@ -90,22 +80,23 @@ def search_test_results(
 ) -> list[float]:
     """Search Test Results by Patient ID.
 
-    Retriving indexes from a list of headers takes time O(CL) each time where
-    CL is the length of the lab file header. Looping through the lab records to
-    check if a patient has completed a specific test takes 0(M**2) time. Then,
-    retriving test results take O(1) constant time. Our big-O notation is
-    therefore O(M**2) after dropping the constant factors.
+    Retriving indexes from a list of headers takes O(1) constant time.
+    Looping through the lab records to check if a patient has completed a
+    specific test takes 0(M*CL) time. Then, retriving test results take O(1)
+    constant time. Our big-O notation is therefore O(M*CL) after dropping the
+    constant factors.
 
     """
     lab_record_header = lab_records[0]["PatientID"]  # O(CL)
-    lab_name_index = lab_record_header.index("LabName")  # O(CL)
-    lab_value_index = lab_record_header.index("LabValue")  # O(CL)
+    lab_name_index = lab_record_header.index("LabName")  # O(1)
+    lab_value_index = lab_record_header.index("LabValue")  # O(1)
     return [
-        float(i[patient_id][lab_value_index])  # O(1)
-        for i in lab_records  # O(M)
+        float(lab_record[patient_id][lab_value_index])  # O(1)
+        for lab_record in lab_records  # O(M)
         if (
-            patient_id in i and i[patient_id][lab_name_index] == test_name
-        )  # O(M)
+            patient_id in lab_record
+            and lab_record[patient_id][lab_name_index] == test_name
+        )  # O(CL)
     ]
 
 
@@ -145,6 +136,7 @@ if __name__ == "__main__":
     records = parse_data(
         "PatientCorePopulatedTable.txt", "LabsCorePopulatedTable.txt"
     )
+    print(patient_age(records[0], "1A8791E3-A61C-455A-8DEE-763EB90C9B2"))
     print(
         patient_is_sick(
             records[1],
