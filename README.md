f499e3057222557979ecf23aad5b975d43abe38c
heath3rq <105904149+heath3rq@users.noreply.github.com>
2023-03-16 17:46:32 +0000

    Replace dict-based data structures with custom classes, and update tests and documentations

--- README.md
+++ README.md
@@ -6,64 +6,53 @@ The ehr-utils library provides analytical capabilities for EHR data.
 * In your local terminal, clone the repo: 
 ```git clone https://github.com/biostat821-2023/ehr-utils-heath3rq.git```
 * Python version: `3.10 and above`
-* Required libraries: `datatime`
+* Required libraries: `datatime`, `pytest`
 
 
 ### Details of Each Function and Expected Input
-The `main.py` includes three main functions and two helper functions:
+The `main.py` includes two custom classes and three functions:
+
+* Class `Lab` storing lab information of a patient as instance attributes
+
+* Class `Patient` storing patient information as instance attributes. The `Patient` class has two properties (`age` and `age_at_first_admission`) and a method called `is_sick` to check whether a patient has ever been sick:
+    - `age`: returns the age in years of a given patient.
+    - `age_at_first_admission` returns the age of a given patient when their earliest lab was recorded. 
+    - `patient_is_sick`: checks whether a patient has ever had a test with value above (">") or below ("<") the given level
+        -  Input: 
+            a. Patient ID in string format
+            b. Lab test name of interest in string format
+            c. A number as the decision criteria of test values
+        - Output: `True` or `False` indcating whether a patient has ever been sick
+
 * `parse_data` reads and parses the data files.
     - Input: 
         a. A path to a `.txt` file storing patient information where the first column is the patient ID. 
         b. A path to a `.txt` file storing lab information of patients where the first column is the patient ID. 
-    - Output: a tuple of two dictionaries, one for patient information and the other for lab information of patients. 
+    - Output: a dictionary with Patient IDs as keys and the `Patient` class as values
 
 * `date_type_conversion` converts date in string format to datetime format. It is used internally in `patient_age` method.
     - Input: date of birth in string format
     - Output: date of birth in date format
 
-* `patient_age` calculates the age in years of a given patient. 
-    - Input: 
-        a. Patient information stored in dictionary format. This is the first dictionary in the resulting tuple from `parse_date` function. 
-        b. Patient ID in string format
-    - Output: the age of a given patient
-
 * `search_test_results`: searches test results of a given patient. It is used internally in `patient_is_sick` method.
     - Input: 
-        a. Lab information of patients stored in dictionary format. This is the second dictionary in the resulting tuple from `parse_date` function. 
-        b. Patient ID in string format
-        c. Lab test name of interest in string format
-    - Output: 
-
-* `patient_is_sick`: checks whether a patient has ever had a test with value above (">") or below ("<") the given level
-    - Input: 
-        a. Lab information of patients stored in dictionary format. This is the second dictionary in the resulting tuple from `parse_date` function. 
-        b. Patient ID in string format
-        c. Lab test name of interest in string format
-        d. A number as the decision criteria of test values
-    - Output: `True` or `False` indcating whether a patient has ever been sick
-
-* `patient_age_at_first_admission`: computes the age of a given patient when their earliest lab was recorded. 
+        a. Lab information of patients stored in the `Lab` class, child of the `Patient` class
+        b. Lab test name of interest in string format
+    - Output: A list of all lab results of a given patient 
 
 
 ### Examples
 ```python
->> patient_records, lab_records = parse_data("patient_file_name.txt", "labs_file_name.txt")
+>> patient_records = parse_data("patient_file_name.txt", "labs_file_name.txt")
 
->> dob_string = "1947-12-28 02:45:40.547"
->> DOB = date_type_conversion(dob_string)
-1947-12-28 02:45:40.547000
-
->> patient_age(patient_records, "1A8791E3-A61C-455A-8DEE-763EB90C9B2C")
+>> patient_records['1A8791E3-A61C-455A-8DEE-763EB90C9B2C'].age
 50
 
->> ssearch_test_results(lab_records, "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F", "URINALYSIS: RED BLOOD CELLS")
-[3.1, 0.8, 1.1, 2.7, 0.1, 1.4, 2.7, 1.9, 1.9, 3.3, 2.7, 2.2, 0.9, 0.5, 1.9, 1.9, 2.2, 0.4]
-
->> patient_is_sick(lab_records, "1A8791E3-A61C-455A-8DEE-763EB90C9B2C", "METABOLIC: ALBUMIN", ">", 4.0)
-True
-
->>> patient_age_at_first_admission(lab_records, patient_records, "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F")
+>>> patient_records['1A8791E3-A61C-455A-8DEE-763EB90C9B2C'].age_at_first_admission
 19
+
+>> patient_records['1A8791E3-A61C-455A-8DEE-763EB90C9B2C'].is_sick("<","URINALYSIS: RED BLOOD CELLS",1.5)
+True
 ```
 
 ### Test Instruction
@@ -72,6 +61,6 @@ To run pytest and check test coverage, run the following code in terminal:
 ```
 pip install --upgrade pip && pip install -r requirements-test.txt
 pytest main_test.py
-coverage run -m pytest main_test.py > test_report.txt
+coverage run -m pytest tests/test_main.py > test_report.txt
 coverage report -m
 ```
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,54 +3,10 @@ import pytest
 from fake_files import fake_files
 from main import (
     parse_data,
-    patient_age,
-    patient_is_sick,
-    patient_age_at_first_admission,
 )
 
 
-lab_records_dict = {
-    "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": [
-        {
-            "AdmissionID": "1",
-            "LabName": "URINALYSIS: RED BLOOD CELLS",
-            "LabValue": "1.8",
-            "LabUnits": "rbc/hpf",
-            "LabDateTime": "1992-07-01 01:36:17.910",
-        },
-    ],
-    "1A8791E3-A61C-455A-8DEE-763EB90C9B2F": [
-        {
-            "AdmissionID": "1",
-            "LabName": "URINALYSIS: RED BLOOD CELLS",
-            "LabValue": "6.8",
-            "LabUnits": "rbc/hpf",
-            "LabDateTime": "1990-08-01 01:34:17.910",
-        },
-    ],
-}
-
-patient_records_dict = {
-    "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": {
-        "PatientGender": "Male",
-        "PatientDateOfBirth": "1973-08-16 10:58:34.413",
-        "PatientRace": "Asian",
-        "PatientMaritalStatus": "Single",
-        "PatientLanguage": "English",
-        "PatientPopulationPercentageBelowPoverty": "13.97",
-    },
-    "1A8791E3-A61C-455A-8DEE-763EB90C9B2O": {
-        "PatientGender": "Male",
-        "PatientDateOfBirth": "1970-07-20 11:08:25.413",
-        "PatientRace": "White",
-        "PatientMaritalStatus": "Divorced",
-        "PatientLanguage": "American",
-        "PatientPopulationPercentageBelowPoverty": "10.23",
-    },
-}
-
-
-def test_parse_data() -> None:
+def test_main() -> None:
     """Test parse data function."""
     table_lab = [
         [
@@ -91,99 +47,27 @@ def test_parse_data() -> None:
         ],
     ]
     with fake_files(table_lab) as labs, fake_files(table_patient) as patients:
-        patient_records, lab_records = parse_data(patients[0], labs[0])
-        assert patient_records == {
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": {
-                "PatientGender": "Male",
-                "PatientDateOfBirth": "1973-08-16 10:58:34.413",
-                "PatientRace": "Asian",
-                "PatientMaritalStatus": "Single",
-                "PatientLanguage": "English",
-                "PatientPopulationPercentageBelowPoverty": "13.97",
-            }
-        }, "Error parsing patient file."
-
-        assert lab_records == {
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": [
-                {
-                    "AdmissionID": "1",
-                    "LabName": "URINALYSIS: RED BLOOD CELLS",
-                    "LabValue": "1.8",
-                    "LabUnits": "rbc/hpf",
-                    "LabDateTime": "1992-07-01 01:36:17.910",
-                },
-            ]
-        }, "Error parsing lab file."
-
-
-def test_patient_age() -> None:
-    """Test patient age function."""
-    patient_records = patient_records_dict
-    patient_age_ = patient_age(
-        patient_records, "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
-    )
-    assert patient_age_ == 50, "Error calculating patient age."
-    with pytest.raises(ValueError):
-        patient_age(
-            patient_records_dict, "FB2ABB23-C9D0-4D09-8464-49BF0B982F0FBB"
-        )
-
-
-def test_patient_age_at_first_admission() -> None:
-    """Test patient age at first admission function"""
-    age_at_admin = patient_age_at_first_admission(
-        patient_records_dict,
-        lab_records_dict,
-        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-    )
+        patient_records = parse_data(patients[0], labs[0])
     assert (
-        age_at_admin == 19
-    ), "Error in patient_age_at_first_admission function"
+        patient_records["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"].age == 50
+    ), "Error calculating patient age."
+    assert (
+        patient_records[
+            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
+        ].age_at_first_admission
+        == 19
+    ), "Error calculating age at first admission"
+    assert (
+        patient_records["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"].is_sick(
+            "<", "URINALYSIS: RED BLOOD CELLS", 1.5
+        )
+        is False
+    ), "Error in determining whether patient is sick."
     with pytest.raises(ValueError):
-        patient_age_at_first_admission(
-            patient_records_dict,
-            lab_records_dict,
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C123",
+        patient_records["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"].is_sick(
+            "=", "URINALYSIS: RED BLOOD CELLS", 1.5
         )
     with pytest.raises(ValueError):
-        patient_age_at_first_admission(
-            patient_records_dict,
-            lab_records_dict,
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2O",
-        )
-
-
-def test_patient_is_sick() -> None:
-    """Test patient_is_sick function."""
-    sick = patient_is_sick(
-        lab_records_dict,
-        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-        "URINALYSIS: RED BLOOD CELLS",
-        "<",
-        1.5,
-    )
-    assert sick is False, "Error in patient_is_sick method."
-    with pytest.raises(ValueError):
-        patient_is_sick(
-            lab_records_dict,
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-            "URINALYSIS: RED BLOOD",
-            "<",
-            1.5,
-        )
-    with pytest.raises(ValueError):
-        patient_is_sick(
-            lab_records_dict,
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2CA",
-            "URINALYSIS: RED BLOOD CELLS",
-            "<",
-            1.5,
-        )
-    with pytest.raises(ValueError):
-        patient_is_sick(
-            lab_records_dict,
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-            "URINALYSIS: RED BLOOD CELLS",
-            "=",
-            1.5,
+        patient_records["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"].is_sick(
+            "<", "URINALYSIS: RED BLOOD CELLSS", 1.5
         )
