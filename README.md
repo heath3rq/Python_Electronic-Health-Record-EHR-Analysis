13ffb986a04fd9a92632720ec4738de5defe64e9
heath3rq <105904149+heath3rq@users.noreply.github.com>
2023-03-25 20:11:57 -0400

    refactor implementation to use sqlite3 (#14)
    * add sql
    
    * add tests and update readme
    
    * update readme details
    
    * fix error
    
    * refine readme
    
    * update main based on feedback
    
    * Update parse data test
    
    * reformat test file
    
    * format test file
    
    * changes
    
    * update gitignore
    
    * update tests
    
    * remove sample db
    
    * restructure Lab class
    
    * fix typo
    
    * Update lab dates
    
    Co-authored-by: Patrick Wang <1263870+patrickkwang@users.noreply.github.com>
    
    * update lab values and dates lists
    
    ---------
    
    Co-authored-by: Patrick Wang <1263870+patrickkwang@users.noreply.github.com>

--- README.md
+++ README.md
@@ -3,55 +3,52 @@
 The ehr-utils library provides analytical capabilities for EHR data.
 
 ### Setup Instructions
-* In your local terminal, clone the repo: 
+- [ ] In your local terminal, clone the repo: 
 ```git clone https://github.com/biostat821-2023/ehr-utils-heath3rq.git```
-* Python version: `3.10 and above`
-* Required libraries: `datatime`, `dataclasses`, `pytest`
+- [ ] Python version: `3.10 and above`
+- [ ] Required library: `pytest`
 
 
 ### Details of Each Function/Class
-The `main.py` includes two custom classes and three functions:
+The `main.py` includes two custom classes and two functions:
 
-* Class `Lab` storing lab information of a patient as instance attributes
+* Class `Lab` storing lab information of a patient as instance attributes by calling the SQL database.
 
-* Class `Patient` storing patient information as instance attributes. The `Patient` class has two properties (`age` and `age_at_first_admission`) and a method called `is_sick` to check whether a patient has ever been sick:
+* Class `Patient` storing patient information as instance attributes by calling the SQL database. The `Patient` class has two properties (`age` and `age_at_first_admission`) and a method called `is_sick` to check whether a patient has ever been sick:
     - `age`: returns the age in years of a given patient.
     - `age_at_first_admission` returns the age of a given patient when their earliest lab was recorded. 
     - `patient_is_sick`: checks whether a patient has ever had a test with value above (">") or below ("<") the given level
-        -  Input:  
-            (a) Patient ID in string format
-            (b) Lab test name of interest in string format
-            (c) A number as the decision criteria of test values
-        - Output: `True` or `False` indcating whether a patient has ever been sick
+        +  Input:  
+            - Patient ID in string format
+            - Lab test name of interest in string format
+            - A number as the decision criteria of test values
+        + Output: `True` or `False` indcating whether a patient has ever been sick
 
-* `parse_data` reads and parses the data files.
+* `parse_data` reads and parses the data files into a SQL database.
     - Input: 
-        (a) A path to a `.txt` file storing patient information where the first column is the patient ID. 
-        (b) A path to a `.txt` file storing lab information of patients where the first column is the patient ID. 
-    - Output: a dictionary with Patient IDs as keys and the `Patient` class as values
+        + A path to a `.txt` file storing patient information where the first column is the patient ID. 
+        + A path to a `.txt` file storing lab information of patients where the first column is the patient ID. 
+        + A path to a database
+    - Output: This function doesn't return any value. Instead, it populates a SQL database with patient and lab information in the background. The database is updated based on the data provided as input to the function, and no direct output is generated.
 
-* `date_type_conversion` converts date in string format to datetime format. It is used internally in `patient_age` method.
+* `date_type_conversion` converts date in string format to datetime format. It is used internally in `age` and `age_at_first_admission` methods.
     - Input: date of birth in string format
     - Output: date of birth in date format
-
-* `search_test_results` searches test results of a given patient. It is used internally in `patient_is_sick` method.
-    - Input: 
-        (a) Lab information of patients stored in the `Lab` class, child of the `Patient` class
-        (b) Lab test name of interest in string format
-    - Output: A list of all lab results of a given patient 
 
 
 ### Examples
 ```python
->> patient_records = parse_data("patient_file_name.txt", "labs_file_name.txt")
+>> from main import parse_data, Patient
+>> parse_data("patient_file_name.txt", "labs_file_name.txt", "SampleDB.db")
 
->> patient_records['1A8791E3-A61C-455A-8DEE-763EB90C9B2C'].age
+>> patient = Patient("1A8791E3-A61C-455A-8DEE-763EB90C9B2C", "SampleDB.db")
+>> patient.age
 50
 
->>> patient_records['1A8791E3-A61C-455A-8DEE-763EB90C9B2C'].age_at_first_admission
+>>> patient.age_at_first_admission
 19
 
->> patient_records['1A8791E3-A61C-455A-8DEE-763EB90C9B2C'].is_sick("<","URINALYSIS: RED BLOOD CELLS",1.5)
+>> patient.is_sick("<","URINALYSIS: RED BLOOD CELLS",1.5)
 True
 ```
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,101 +1,122 @@
 """A module that tests EHR data."""
+import sqlite3
 import pytest
 from fake_files import fake_files
-from main import parse_data, Patient, Lab
+from main import parse_data, Patient
 
-lab_records = [
-    Lab(
+
+DATABASE_TEST = "SampleDB_Test.db"
+
+table_lab = [
+    [
+        "PatientID",
+        "AdmissionID",
+        "LabName",
+        "LabValue",
+        "LabUnits",
+        "LabDateTime",
+    ],
+    [
         "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+        "1",
         "URINALYSIS: RED BLOOD CELLS",
         "1.8",
+        "rbc/hpf",
         "1992-07-01 01:36:17.910",
-    ),
+    ],
+]
+table_patient = [
+    [
+        "PatientID",
+        "PatientGender",
+        "PatientDateOfBirth",
+        "PatientRace",
+        "PatientMaritalStatus",
+        "PatientLanguage",
+        "PatientPopulationPercentageBelowPoverty",
+    ],
+    [
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+        "Male",
+        "1973-08-16 10:58:34.413",
+        "Asian",
+        "Single",
+        "English",
+        "13.97",
+    ],
 ]
 
-patient_records = {
-    "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": Patient(
-        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-        "1973-08-16 10:58:34.413",
-        lab_records,
-    )
-}
 
-
-def test_main() -> None:
+def test_parse_data() -> None:
     """Test parse data function."""
-    table_lab = [
-        [
-            "PatientID",
-            "AdmissionID",
-            "LabName",
-            "LabValue",
-            "LabUnits",
-            "LabDateTime",
-        ],
-        [
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-            "1",
-            "URINALYSIS: RED BLOOD CELLS",
-            "1.8",
-            "rbc/hpf",
-            "1992-07-01 01:36:17.910",
-        ],
-    ]
-    table_patient = [
-        [
-            "PatientID",
-            "PatientGender",
-            "PatientDateOfBirth",
-            "PatientRace",
-            "PatientMaritalStatus",
-            "PatientLanguage",
-            "PatientPopulationPercentageBelowPoverty",
-        ],
-        [
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-            "Male",
-            "1973-08-16 10:58:34.413",
-            "Asian",
-            "Single",
-            "English",
-            "13.97",
-        ],
-    ]
-    with fake_files(table_lab) as labs, fake_files(table_patient) as patients:
-        records = parse_data(patients[0], labs[0])
-    assert records == patient_records, "Error parsing data."
+    with fake_files(table_lab) as _labs, fake_files(
+        table_patient
+    ) as _patients:
+        parse_data(_patients[0], _labs[0], "test.db")
+        # Check if the expected data was inserted into the database
+        con = sqlite3.connect("test.db")
+        with con as cursor:
+            patient_db = cursor.execute(
+                "SELECT id, date_of_birth FROM patients"
+            ).fetchall()
+            assert (
+                patient_db[0][0] == "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
+            ), "Error parsing the patient file, patient id did not insert \
+                correctly."
+            assert (
+                patient_db[0][1] == "1973-08-16 10:58:34.413"
+            ), "Error parsing the patient file, patient date of birth did not \
+                insert correctly."
+            labs_db = cursor.execute(
+                "SELECT patient_id, lab_name, lab_value, lab_date FROM labs"
+            ).fetchall()
+            assert (
+                labs_db[0][0] == "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
+            ), "Error parsing the lab file, patient id did not insert \
+                correctly."
+            assert (
+                labs_db[0][1] == "URINALYSIS: RED BLOOD CELLS"
+            ), "Error parsing the lab file, lab value did not insert \
+                correctly."
+            assert (
+                labs_db[0][2] == "1.8"
+            ), "Error parsing the lab file, lab value did not insert \
+                correctly."
+            assert (
+                labs_db[0][3] == "1992-07-01 01:36:17.910"
+            ), "Error parsing the lab file, lab date did not insert correctly."
+
+        with pytest.raises(FileNotFoundError):
+            parse_data("patients[0].txt", _labs[0], "test.db")
+        with pytest.raises(FileNotFoundError):
+            parse_data(_patients[0], "labs[0].txt", "test.db")
+
+
+with fake_files(table_lab) as labs, fake_files(table_patient) as patients:
+    parse_data(patients[0], labs[0], DATABASE_TEST)
+
+
+patient = Patient("1A8791E3-A61C-455A-8DEE-763EB90C9B2C", DATABASE_TEST)
 
 
 def test_age() -> None:
     """Test age property in Patient class."""
-    assert (
-        patient_records["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"].age == 50
-    ), "Error calculating patient age."
+    assert patient.age == 50, "Error calculating patient age."
 
 
 def test_age_at_first_admission() -> None:
     """Test age at first admission property in Patient class."""
     assert (
-        patient_records[
-            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
-        ].age_at_first_admission
-        == 19
+        patient.age_at_first_admission == 19
     ), "Error calculating age at first admission"
 
 
 def test_is_sick() -> None:
     """Test is sick method in Patient class"""
     assert (
-        patient_records["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"].is_sick(
-            "<", "URINALYSIS: RED BLOOD CELLS", 1.5
-        )
-        is False
+        patient.is_sick("<", "URINALYSIS: RED BLOOD CELLS", 1.5) is False
     ), "Error in determining whether patient is sick."
     with pytest.raises(ValueError):
-        patient_records["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"].is_sick(
-            "=", "URINALYSIS: RED BLOOD CELLS", 1.5
-        )
+        patient.is_sick("=", "URINALYSIS: RED BLOOD CELLS", 1.5)
     with pytest.raises(ValueError):
-        patient_records["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"].is_sick(
-            "<", "URINALYSIS: RED BLOOD CELLSS", 1.5
-        )
+        patient.is_sick("<", "URINALYSIS: RED BLOOD CELLSS", 1.5)
